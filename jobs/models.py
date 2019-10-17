a79c6c37dce8cfedf8457d129172b8a04d2dd222
KevinCurtisDev <kpcurtis@gmail.com>
2019-10-17 11:07:25 +0100

    Created Job model database schema for job listings

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
+from recruiters.models import Recruiter
 
-# Create your models here.
+class Job(models.Model):
+    recruiter = models.ForeignKey(Recruiter, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    job_type = models.CharField(max_length=200)
+    description = models.TextField(max_length=800)
+    salary = models.IntegerField()
+    listed_date = models.DateTimeField(default=datetime.now, blank=True)
+    added = models.BooleanField(default=True)
+    def __str__(self):
+        return self.title
+
