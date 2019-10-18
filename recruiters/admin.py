68bf384345a7635c60b93ff179f6f689d3eebe7e
KevinCurtisDev <kpcurtis@gmail.com>
2019-10-18 23:00:32 +0100

    Customised admin recruiter area

--- recruiters/admin.py
+++ recruiters/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Recruiter
 
-admin.site.register(Recruiter)
+class RecruiterAdmin(admin.ModelAdmin):
+    # Add fields to the admin Recruiter area
+    list_display = ('id', 'name', 'email')
+    # Make the the recruiter ID and Name a clickable link
+    list_display_links = ('id', 'name')
+    # Add pagination to admin recruiter listing
+    list_per_page = 10
+
+admin.site.register(Recruiter, RecruiterAdmin)
