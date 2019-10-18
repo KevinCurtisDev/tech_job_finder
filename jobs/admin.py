0d000e2c0d5b8c25ab46f2463dbcf50db314e055
KevinCurtisDev <kpcurtis@gmail.com>
2019-10-18 22:55:36 +0100

    Customised admin job listings area

--- jobs/admin.py
+++ jobs/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Job
 
-admin.site.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    # Add fields to the admin job area
+    list_display = ('id', 'title', 'recruiter')
+    # Make the the job ID and Title a clickable link
+    list_display_links = ('id', 'title')
+    # Add pagination to admin job listing
+    list_per_page = 10
+
+admin.site.register(Job, JobAdmin)
