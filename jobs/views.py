ca7e6ab9f5100d6f414a56cce68769fae473fc58
KevinCurtisDev <kpcurtis@gmail.com>
2019-10-19 00:58:51 +0100

    populated job listings markup with dynamic content from the database

--- jobs/views.py
+++ jobs/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
+from .models import Job
+
 def index(request):
-    return render(request, 'jobs/jobs.html')
+    jobs = Job.objects.all()
+
+    context = {
+        'jobs': jobs
+    }
+
+    return render(request, 'jobs/jobs.html', context)
 
 
-def job(request):
+def job(request, job_id):
     return render(request, 'jobs/job.html')
 
 
