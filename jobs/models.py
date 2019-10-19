d7892069caacf7e0a869a8e06bdd87a45b4739f1
KevinCurtisDev <kpcurtis@gmail.com>
2019-10-19 03:51:30 +0100

    Added pagination logic and markup to job listing page

--- jobs/models.py
+++ jobs/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from recruiters.models import Recruiter
 
 class Job(models.Model):
+    #Create database fields for individual jobs
     recruiter = models.ForeignKey(Recruiter, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
@@ -10,7 +11,6 @@ class Job(models.Model):
     description = models.TextField(max_length=800)
     salary = models.IntegerField()
     listed_date = models.DateTimeField(default=datetime.now, blank=True)
-    added = models.BooleanField(default=True)
     def __str__(self):
         return self.title
 
--- jobs/views.py
+++ jobs/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, Paginator
 
 from .models import Job
 
 def index(request):
+    # set job variable to hold job info from database
     jobs = Job.objects.all()
 
+    # set nuber of jobs to be listed per page 
+    paginator = Paginator(jobs, 2)
+    page = request.GET.get('page')
+    paged_jobs = paginator.get_page(page)
+
+    # context variable holds data to be inserted into the html markup
     context = {
-        'jobs': jobs
+        'jobs': paged_jobs
     }
 
+    # return the jobs markup and make available the database job data
     return render(request, 'jobs/jobs.html', context)
 
 
 def job(request, job_id):
+    # return a dynamically created url for individual job listings
     return render(request, 'jobs/job.html')
 
 
