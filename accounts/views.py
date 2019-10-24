46e00eef557befe6739f670b4d943c1ea9a1c53a
KevinCurtisDev <kpcurtis@gmail.com>
2019-10-24 12:14:55 +0100

    Added register form validations and error messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,43 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 def login(request):
     return render(request, 'accounts/login.html')
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    '''
+        This function takes the registration post request variables and checks them against
+        custom validations. If the validations pass then a new user is created and the user is taken
+        to the login page. Otherwise the user is redirected to the register page with a validation error message.
+    '''
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                print('That name is already registered')
+                messages.error(request, 'That name is already registered')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    print ('That email is already registered to a user')
+                    messages.error(request, 'That email is already registered to a user')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, email=email, password=password)
+                    user.save()
+                    print ('You are now registered')
+                    messages.success(request, 'You are now registered')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
 
 def logout(request):
     return redirect(request, 'index')
--- jobfinder/settings.py
+++ jobfinder/settings.py
@@ -134,3 +134,8 @@ STATICFILES_DIRS = [
 # Media Folder settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: ''
+}
